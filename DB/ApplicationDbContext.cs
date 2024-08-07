7bdd743d2c8d85c3a1252c4ccb21669bdf9933e8
Nik <nikitozer25@gmail.com>
2024-08-07 20:15:21 +0700

    .

--- DB/ApplicationDbContext.cs
+++ DB/ApplicationDbContext.cs
@@ -1,6 +1,20 @@
-﻿namespace Prod.DB
+﻿using Microsoft.EntityFrameworkCore;
+using Prod.Entity;
+
+namespace Prod.DB
+
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public DbSet<ProductInfo> ProductInfos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
     }
 }
